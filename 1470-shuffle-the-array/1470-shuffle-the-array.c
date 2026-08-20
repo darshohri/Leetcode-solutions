0170310fec4d8091dca6edb53aa1b5bbc682ced9
Darsh Ohri <DARSH.OHRI061@nmims.in>
2026-08-20 22:28:07 +0530

    Time: 6 ms (98.52%), Space: 12.3 MB (66.76%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.c
@@ -0,0 +1,13 @@
+
+int* shuffle(int* nums, int numsSize, int n, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+    
+    for (int i = 0; i < n; i++) 
+    {
+        result[2 * i] = nums[i];         
+        result[2 * i + 1] = nums[i + n]; 
+    }
+    
+    return result;
+}
