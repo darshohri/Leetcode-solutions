dbffb7092cfeac643df26904585ceb14c8a96706
Darsh Ohri <DARSH.OHRI061@nmims.in>
2026-08-25 02:08:05 +0530

    Time: 0 ms (100%), Space: 8.9 MB (55.83%) - LeetHub

---
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.c
@@ -0,0 +1,11 @@
+int differenceOfSums(int n, int m) {
+    int diff = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % m == 0) {
+            diff -= i;
+        } else {
+            diff += i;
+        }
+    }
+    return diff;
+}
